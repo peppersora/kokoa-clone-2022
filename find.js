50105fe729583c33dfc18a01eb9ccea2456d5a01
peppersora <110614387+peppersora@users.noreply.github.com>
2022-11-03 21:57:00 +0900

    likeCount
    clicked and count++

--- find.js
+++ find.js
@@ -12,8 +12,27 @@ function Putclick(){
     invite.classList.remove(HIDDEN_CLASSNAME);
 
 }
-
-
-
 envelope.addEventListener("click",Putclick);
+
+
+//클릭하면 좋아요 수 증가..!
+//1. 가져오기
+const heart = document.querySelector(".fa-heart");
+let count = document.querySelector(".open-post_heart-column>span");
+// console.log(heart);
+// console.log(count);
+
+//2. 하트를 클릭하면 수가 증가
+let val=count.innerHTML;
+function Heartclick(){
+    // console.log("clicked!");
+    val++;
+    console.log(val);
+    count.innerHTML=val;
+}
+
+
+heart.addEventListener("click",Heartclick);
+
+
 
